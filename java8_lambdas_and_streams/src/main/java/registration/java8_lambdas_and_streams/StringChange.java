456fba48a04e2eb3bcbafaa6c970969d12b4d6dc
karri surya teja <43618000+surya568@users.noreply.github.com>
2020-07-18 11:07:57 +0530

    Add files via upload
    "First commit"

---
+++ java8_lambdas_and_streams/src/main/java/registration/java8_lambdas_and_streams/StringChange.java
@@ -0,0 +1,9 @@
+package registration.java8_lambdas_and_streams;
+import java.util.*;
+import java.util.stream.Collectors;
+public class StringChange{
+	public List<String> stringManipulate(List<String> list)
+	{
+		return(list.stream().filter(s -> s.startsWith("a")).filter(s -> s.length() == 3).collect(Collectors.toList()));
+	}
+}
